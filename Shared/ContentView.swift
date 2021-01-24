084ca53a1576b40981459bc3483fcffa8392e7af
Edwin Torres <edwin_torres@icloud.com>
2021-01-24 15:03:40 -0500

    Started UI wireframing.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,10 +7,50 @@
 
 import SwiftUI
 
+enum ClaculatorButton: String {
+    
+    case zero, one, two, three, four, five, six, seven, eight, convenience
+    case eqwuals, plus, minus, multiply, divide
+    case C
+}
+
 struct ContentView: View {
+    
+    let buttons: [[ClaculatorButton]] = [
+        [.one, .two, .three, .plus]
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack (alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(spacing: 12){
+                
+                HStack {
+                    Spacer()
+                    Text("42").foregroundColor(.white)
+                        .font(.system(size: 64))
+                }.padding()
+            
+                ForEach(buttons, id: \.self) {row in
+                    HStack (spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            Text(button.rawValue)
+                            .font(.system(size:32))
+                            .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                            .foregroundColor(.white)
+                            .background(Color.yellow)
+                            .cornerRadius(self.buttonWidth())
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
+    }
+    
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
