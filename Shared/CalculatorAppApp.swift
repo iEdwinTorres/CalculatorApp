fbfd48bf91d3b6dce5c2d55301e9d29e3502d777
Edwin Torres <edwin_torres@icloud.com>
2021-01-24 13:46:56 -0500

    Initial Commit

---
+++ Shared/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  Shared
+//
+//  Created by Edwin Torres on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
